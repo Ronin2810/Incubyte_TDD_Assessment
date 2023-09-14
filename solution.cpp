22f3906d1ce87dce8de114ac8fdc6e62ccaf1cd9
Ronin2810 <adityapatilsy@gmail.com>
2023-09-14 19:13:08 +0530

    variable and input check

--- solution.cpp
+++ solution.cpp
@@ -4,17 +4,21 @@ using namespace std;
 
 int main(){
     char dir;
-    int coord[3];
+    int x,y,z;
     string cmd;
     
     cout<<"Enter starting coordinates (space separated (x,y,z)):"<<endl;
-    cin>>coord[0]>>coord[1]>>coord[2];
+    cin>>x>>y>>z;
     cout<<"Enter start face direction (N,S,E,W,U,D)"<<endl;
     cin>>dir;
     cout<<"Enter commands to be implemented (continuous)";
     cin>>cmd;
 
+    cout<<"Facing Direction: "<<dir<<endl;
+    cout<<"Starting Coordinates: "<<"("<<x<<","<<y<<","<<z<<")"<<endl;
+    cout<<"Commands: "<<cmd<<endl;
     
+
 
 
 
