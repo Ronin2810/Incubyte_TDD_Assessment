6cb20225121eb54f8a7cca01f479f12ef7438d6a
Ronin2810 <adityapatilsy@gmail.com>
2023-09-14 19:08:47 +0530

    first commit

---
+++ solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    char dir;
+    int coord[3];
+    string cmd;
+    
+    cout<<"Enter starting coordinates (space separated (x,y,z)):"<<endl;
+    cin>>coord[0]>>coord[1]>>coord[2];
+    cout<<"Enter start face direction (N,S,E,W,U,D)"<<endl;
+    cin>>dir;
+    cout<<"Enter commands to be implemented (continuous)";
+    cin>>cmd;
+
+    
+
+
+
+    return 0;
+}
